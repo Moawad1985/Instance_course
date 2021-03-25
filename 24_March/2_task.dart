95ba16a4feca55cd092ed74d3344a56197c82752
Moawad1985 <63582796+Moawad1985@users.noreply.github.com>
2021-03-25 22:36:42 +0200

    first commit

---
+++ 24_March/2_task.dart
@@ -0,0 +1,9 @@
+void main() {
+  for (var i = 1; i <= 100; i++) {
+    if (i.isEven) print(i);
+  }
+
+  // for (var i = 1; i <= 100; i++) {
+  //   if (i % 2 == 0) print(i);
+  // }
+}
---
+++ 24_March/4_task.dart
@@ -0,0 +1,11 @@
+void main() {
+  int i = 0;
+  int sum = 0;
+
+  while (i < 100) {
+    i++;
+    sum += i;
+  }
+
+  print(sum);
+}
